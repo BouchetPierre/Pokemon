269be8b30f2c7d5217c7915711a9b82a57aa97c4
juilleratn <juilleratnoel@gmail.com>
2019-07-19 16:24:51 +0200

    login et enregistrement
    Signed-off-by: juilleratn <juilleratnoel@gmail.com>

---
+++ login/verification.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+if(isset($_POST['name']) && isset($_POST['mdp']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = 'ADRAR1112';
+    $db_name     = 'jeu_pokemon';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+
+
+    $username = mysqli_real_escape_string($db,htmlentities($_POST['name']));
+    $password = mysqli_real_escape_string($db,htmlentities($_POST['mdp']));
+
+    if($username !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*)
+                    FROM dresseur
+                    WHERE name = '".$username."' and mdp = '".$password."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // si name et mdp corrects
+        {
+           $_SESSION['name'] = $username;
+           header('Location: ../pokemon_user.html');
+        }
+        else
+        {
+        header('Location: error.html'); //  name ou mdp incorrect
+        }
+    }
+
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
