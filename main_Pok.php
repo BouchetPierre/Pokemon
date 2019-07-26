8949b6a83c53123fc43d1cf45f15d58c983e9ef5
BouchetPierre <bouchet.hp@gmail.com>
2019-07-26 12:15:41 +0200

    fihier main_Pok a jour le 26 juillet à 12h00

--- main_Pok.php
+++ main_Pok.php
@@ -14,7 +14,6 @@ try {
 
 /*------requete et construction Pokemon 1-------*/
 
-
 $affichage_d1= $dresseur->prepare('SELECT name, url_image_d1, pv, fk_id_att1, fk_id_att2, fk_id_att3, fk_id_att4
                                   FROM pokemonDesk
                                   WHERE name = ?');
@@ -22,23 +21,25 @@ $req1 = $affichage_d1->execute(array($_SESSION['pokemon_d1']));
 
 $donneeD1 = $affichage_d1->fetch();
 
+$bindsD1 = [$donneeD1['fk_id_att1'], $donneeD1['fk_id_att2'], $donneeD1['fk_id_att3'], $donneeD1['fk_id_att4']];
+$bindsD1Prepared = implode(',', $bindsD1);
 
-$attaq_d1= $dresseur->prepare('SELECT name
-                                  FROM typeAttaque
-                                  WHERE id_typeAttaque = ? AND id_typeAttaque = ? AND id_typeAttaque = ? AND id_typeAttaque = ? ');
-$reqAt1 = $attaq_d1->execute(array($donneeD1['fk_id_att1'], $donneeD1['fk_id_att2'], $donneeD1['fk_id_att3'], $donneeD1['fk_id_att4']));
+$attaq_d1 = $dresseur->query("
+          SELECT name FROM typeAttaque
+            WHERE id_typeAttaque
+            IN (".$bindsD1Prepared.")
+          ");
 
-$donneeAttD1 = $attaq_d1->fetch();
-var_dump($donneeAttD1);
+$resultsD1 = $attaq_d1->fetchAll();
 
+$tabResultD1;
+foreach($resultsD1 as $result){
+  $tabResultD1[] = ($result['name']);
+}
 
-
-$pokemonDres_1 = new Pokemon($donneeD1['name'], $donneeD1['url_image_d1'], $donneeD1['pv'], $donneeAttD1[0], $donneeAttD1[1], $donneeAttD1[2], $donneeAttD1[3]);
-var_dump($pokemonDres_1).'<br>';
-
+$pokemonDres_1 = new Pokemon($donneeD1['name'], $donneeD1['url_image_d1'], $donneeD1['pv'], $tabResultD1[0], $tabResultD1[1], $tabResultD1[2], $tabResultD1[3]);
 
 /*------requete et construction Pokemon 2-------*/
-
 
 $_SESSION['pokemon_d2']=$_POST['pokemon_d2'];
 
@@ -49,30 +50,31 @@ $req2 = $affichage_d2->execute(array($_SESSION['pokemon_d2']));
 
 $donneeD2 = $affichage_d2->fetch();
 
+$bindsD2 = [$donneeD2['fk_id_att1'], $donneeD2['fk_id_att2'], $donneeD2['fk_id_att3'], $donneeD2['fk_id_att4']];
+$bindsD2Prepared = implode(',', $bindsD2);
 
-$attaq_d2= $dresseur->prepare('SELECT name
-                                  FROM typeAttaque
-                                  WHERE id_typeAttaque = ? AND id_typeAttaque = ? AND id_typeAttaque = ? AND id_typeAttaque = ? ');
-$reqAt2 = $attaq_d2->execute(array($donneeD2['fk_id_att1'], $donneeD2['fk_id_att2'], $donneeD2['fk_id_att3'], $donneeD2['fk_id_att4']));
+$attaq_d2 = $dresseur->query("
+          SELECT name FROM typeAttaque
+            WHERE id_typeAttaque
+            IN (".$bindsD2Prepared.")
+          ");
 
-$donneeAttD2 = $attaq_d2->fetch();
-var_dump($donneeAttD2);
+$resultsD2 = $attaq_d2->fetchAll();
 
+$tabResultD2;
+foreach($resultsD2 as $result){
+  $tabResultD2[] = ($result['name']);
+}
 
-
-$pokemonDres_2 = new Pokemon($donneeD2['name'], $donneeD2['url_image_d2'], $donneeD2['pv'], $donneeAttD2[0], $donneeAttD2[1], $donneeAttD2[2], $donneeAttD2[3]);
-var_dump($pokemonDres_2).'<br>';
-
-
+$pokemonDres_2 = new Pokemon($donneeD2['name'], $donneeD2['url_image_d2'], $donneeD2['pv'], $tabResultD2[0], $tabResultD2[1], $tabResultD2[2], $tabResultD2[3]);
 
 /*------construction Dresseur------*/
 
 
 $dresseur_d1 = new Dresseur($_SESSION['name_d1']);
-var_dump($dresseur_d1).'<br>';
+
 
 $dresseur_d2 = new Dresseur($_SESSION['name_d2']);
-var_dump($dresseur_d2).'<br>';
 
 
 /*------requete et des attaques du dresseur 1-------*/
